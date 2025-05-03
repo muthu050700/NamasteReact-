352880f8089bfa782cc6eb7bd31f4e84fa95b094
Santhosh Sivaji <santhosh_s@ambattur.com>
2025-05-03 12:24:13 +0530

    eposide-1

---
+++ app.js
@@ -0,0 +1,12 @@
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "Iam a h1 tag"),
+        React.createElement("h2", {}, "Iam a h2 tag")
+    ]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "iam a h1 tag"),
+        React.createElement("h2", {}, "Iam a h2 tag")
+    ])
+])
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
